a42641fd3cadb839eec02f1595959c977ff225db
jcerda <jccer103@gmail.com>
2018-07-07 21:04:47 -0500

    reduced forecasts down to 5 days

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,8 @@ angular.module('5day-forecast')
     templateUrl: 'src/templates/app.html',
     controller: function($window) {
       this.data = $window.data;
-      this.forecasts = data[0].list
+      this.forecasts = data[0].list.sort((a, b) => a.dt_txt.localeCompare(b.dt_txt)).filter((value,index) => index % 8 === 0)
       this.currentCity = data[0].city.name;
-      
+      console.log(this.forecasts)
     }
   });
