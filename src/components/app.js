a8794155ce8067d007a5c690e25fca28d250730e
jcerda <jccer103@gmail.com>
2018-07-09 17:55:29 -0500

    removed console statements

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,6 @@
 angular.module('5day-forecast')
 .component('app', {
   controller: function(forecastSearch) {
-    console.log(window)
     this.searchProvider = forecastSearch;
   	this.searchProvider.search('60661', (forecasts) => {
       // sorts the list to only provide 5 of the 39+ returned 
--- src/components/search.js
+++ src/components/search.js
@@ -1,6 +1,5 @@
 angular.module('5day-forecast')
 .controller('searchController', function(){
-  console.log(window)
   this.handleClick = (query) => {
     this.searchProvider.search(query, forecasts => {
       this.forecasts = forecasts.list.sort((a, b) => a.dt_txt.localeCompare(b.dt_txt)).filter((value,index) => index % 8 === 0);
